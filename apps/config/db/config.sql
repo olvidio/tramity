ba1868097ed16931f1fbc491fd74e6c0118847fe
Daniel Serrabou <dserrabou@gmail.com>
2020-10-28 11:31:30 +0100

    datos db lugares, config

--- apps/config/db/config.sql
+++ apps/config/db/config.sql
@@ -3,10 +3,16 @@ CREATE TABLE IF NOT EXISTS x_config (
     valor text
     );
         
-        
 ALTER TABLE x_config OWNER TO tramity;
 
 ALTER TABLE x_config ADD PRIMARY KEY (parametro);
 
-
-INSERT INTO x_config (parametro, valor) VALUES ('',3);
+INSERT INTO x_config (parametro, valor) VALUES ('ambito',3);
+INSERT INTO x_config (parametro, valor) VALUES ('sigla', 'dlb');
+INSERT INTO x_config (parametro, valor) VALUES ('idioma_default', 'es_ES.UTF-8');
+INSERT INTO x_config (parametro, valor) VALUES ('server_etherpad', 'http://tramity.local:9001');
+INSERT INTO x_config (parametro, valor) VALUES ('plazo_urgente', 1);
+INSERT INTO x_config (parametro, valor) VALUES ('plazo_rapido', 7);
+INSERT INTO x_config (parametro, valor) VALUES ('plazo_desconocido', 56);
+INSERT INTO x_config (parametro, valor) VALUES ('plazo_normal', 14);
+INSERT INTO x_config (parametro, valor) VALUES ('plazo_error', 21);
