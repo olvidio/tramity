1a2554849d379ad9addcfc3efbdd6d5da0c6c77a
dani <dani@dani-ThinkPad-T540p>
2021-06-15 16:36:42 +0200

    quitar menu documentos

--- apps/usuarios/controller/usuario_home.php
+++ apps/usuarios/controller/usuario_home.php
@@ -257,6 +257,7 @@ $filtro = 'pendientes';
     $pill = [ 'orden'=> $num_orden, 'text' => $text, 'pag_lst' => $pag_lst, 'num' => $num, 'active' => $active];
 $a_pills[$num_orden] = $pill;
 
+/*
 // documentos = 40;
 $filtro = 'documentos';
     $active = ($filtro == $Qfiltro)? 'active' : '';
@@ -268,6 +269,7 @@ $filtro = 'documentos';
     $num = '';
     $pill = [ 'orden'=> $num_orden, 'text' => $text, 'pag_lst' => $pag_lst, 'num' => $num, 'active' => $active];
 $a_pills[$num_orden] = $pill;
+*/
 
 // ordenar:
 ksort($a_pills);
