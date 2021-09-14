83264b95242793dd44bbefbcc175129133d5f24b
dani <dani@dani-ThinkPad-T540p>
2021-09-14 13:02:23 +0200

    error en download entradas

--- apps/entradas/controller/download.php
+++ apps/entradas/controller/download.php
@@ -14,47 +14,37 @@ require_once ("apps/core/global_object.inc");
 // El download es via GET!!!";
 
 $Qid_item = (integer) \filter_input(INPUT_GET, 'key');
-$Qplugin = (integer) \filter_input(INPUT_GET, 'plugin');
 
 if (!empty($Qid_item)) {
     $oEntradaAdjunto = new EntradaAdjunto($Qid_item);
-    $res_adjunto = $oEntradaAdjunto->getAdjuntoResource();
     $nom = $oEntradaAdjunto->getNom();
+    $res_adjunto = $oEntradaAdjunto->getAdjuntoResource();
 
-    $extension = '';
-    // Determine Content Type
-    switch ($extension) {
-        case "pdf": $ctype="application/pdf"; break;
-        case "exe": $ctype="application/octet-stream"; break;
-        case "zip": $ctype="application/zip"; break;
-        case "rtf": $ctype="application/msword"; break;
-        case "doc": $ctype="application/msword"; break;
-        case "xls": $ctype="application/vnd.ms-excel"; break;
-        case "ppt": $ctype="application/vnd.ms-powerpoint"; break;
-        case "gif": $ctype="image/gif"; break;
-        case "png": $ctype="image/png"; break;
-        case "jpeg":
-        case "jpg": $ctype="image/jpg"; break;
-        //default: $ctype="application/force-download";
-        default: $ctype="application/octet-stream";
-    }
-    
-    header('Content-Description: File Transfer');
-    header('Content-Transfer-Encoding: binary');
-    header('Cache-Control: public, must-revalidate, max-age=0');
-    header("Pragma: public"); // required
-    header("Expires: 0");
-    header("Cache-Control: private",false); // required for certain browsers
-    header('Content-Type: application/force-download');
-    header('Content-Type: application/octet-stream', false);
-    header('Content-Type: application/download', false);
-    //header("Content-Type: $ctype");
-    //header("Content-Disposition: attachment; filename=\"".basename($fullPath)."\";" );
-    header('Content-disposition: attachment; filename="' . $nom . '"');
-    ob_clean();
-    flush();
     if (!empty($res_adjunto)) {
-        echo fpassthru($res_adjunto);
+        rewind($res_adjunto);
+        $doc_encoded = stream_get_contents($res_adjunto);
+        if ( base64_encode(base64_decode($doc_encoded, true)) === $doc_encoded){
+            //echo '$data is valid';
+            $doc = base64_decode($doc_encoded);
+        } else {
+            //ºecho '$data is NOT valid';
+            $doc = $doc_encoded;
+        }
+        
+        header('Content-Description: File Transfer');
+        header('Content-Transfer-Encoding: binary');
+        header('Cache-Control: public, must-revalidate, max-age=0');
+        header("Pragma: public"); // required
+        header("Expires: 0");
+        header("Cache-Control: private",false); // required for certain browsers
+        header('Content-Type: application/force-download');
+        //header('Content-Type: application/octet-stream', false);
+        header('Content-Type: application/download', false);
+        //header("Content-Disposition: attachment; filename=\"".basename($fullPath)."\";" );
+        header('Content-disposition: attachment; filename="' . $nom . '"');
+        ob_clean();
+        flush();
+        echo $doc;
     }
     exit();
 } else {
