f631dec64672c37b39bcd5723008b9312673a658
dani <dani@dani-ThinkPad-T540p>
2022-03-28 14:06:03 +0200

    debug, que vuelva al enviar

--- apps/escritos/controller/escrito_enviar.php
+++ apps/escritos/controller/escrito_enviar.php
@@ -37,7 +37,6 @@ $oEnviar = new Enviar($Qid_escrito,'escrito');
 $a_rta = $oEnviar->enviar();
 
 if ($a_rta['marcar'] === TRUE) {
-	/*
     $oEscrito = new Escrito($Qid_escrito);
     $oEscrito->DBCarregar();
     $oEscrito->setF_salida($f_salida,FALSE);
@@ -48,7 +47,6 @@ if ($a_rta['marcar'] === TRUE) {
     if ($oEscrito->DBGuardar() === FALSE) {
         exit($oEscrito->getErrorTxt());
     }
-    */
 }
 if ($a_rta['success'] === TRUE) {
     // para que se cierre la ventana que se ha abierto:
