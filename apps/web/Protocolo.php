63b14106b176e21868bb0ffcc03ff29dc5691968
dani <dani@dani-ThinkPad-T540p>
2022-11-25 10:30:53 +0100

    errores varios por tipado

--- apps/web/Protocolo.php
+++ apps/web/Protocolo.php
@@ -26,22 +26,22 @@ class Protocolo
      * id del lugar
      * @var integer|null
      */
-    protected  $ilugar;
+    protected  $ilugar = null;
     /**
      *
      * @var integer|null
      */
-    protected  $iprot_num;
+    protected  $iprot_num = null;
     /**
      * Debe ser un string para permitir "00", "01" etc.
      * @var string|null
      */
-    protected ?string $sprot_any;
+    protected ?string $sprot_any = null;
     /**
      *
      * @var string|null
      */
-    protected  $sprot_mas;
+    protected  $sprot_mas = null;
 
     /* CONSTRUCTOR ------------------------------ */
     function __construct($ilugar = null, $iprot_num = null, $iprot_any = '', $sprot_mas = '')
