47bc58e77a72604d9b8a2f231ef9075ea6c9c93e
dani <dani@dani-ThinkPad-T540p>
2023-05-25 10:17:08 +0200

    migrar: separar menus dl - ctr

--- apps/migration_ctr/controller/migration_ctr_index.php
+++ apps/migration_ctr/controller/migration_ctr_index.php
@@ -1,6 +1,8 @@
 <?php
 
+use config\model\entity\ConfigSchema;
 use core\ViewTwig;
+use usuarios\model\entity\Cargo;
 
 // INICIO Cabecera global de URL de controlador *********************************
 
@@ -22,8 +24,19 @@ if (!empty($a_sel)) { //vengo de un checkbox
     }
 }
 
+
+$oConfigSchema = new ConfigSchema('ambito');
+$valor_ambito = (int)$oConfigSchema->getValor();
+$id_ambito_dl = FALSE;
+$a_roles_posibles = [];
+if ($valor_ambito === Cargo::AMBITO_DL) {
+    $id_ambito_dl = TRUE;
+
+}
+
 $a_campos = [
     'lista_id_ctr' => $str_id_ctr,
+    'is_ambito_dl' => $id_ambito_dl,
 ];
 
 $oView = new ViewTwig('migration_ctr/controller');
