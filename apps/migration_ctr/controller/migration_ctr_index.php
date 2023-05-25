1599561e85a6fe941de6f79a7a4e806407fcb2fb
dani <dani@dani-ThinkPad-T540p>
2023-05-25 10:28:53 +0200

    migrar: quitar separar menus dl - ctr

--- apps/migration_ctr/controller/migration_ctr_index.php
+++ apps/migration_ctr/controller/migration_ctr_index.php
@@ -24,25 +24,15 @@ if (!empty($a_sel)) { //vengo de un checkbox
     }
 }
 
+
 /*
-$oConfigSchema = new ConfigSchema('ambito');
-$valor_ambito = (int)$oConfigSchema->getValor();
-$id_ambito_dl = FALSE;
-$a_roles_posibles = [];
-if ($valor_ambito === Cargo::AMBITO_DL) {
-    $id_ambito_dl = TRUE;
-
-}
-*/
-
-$id_ambito_dl = FALSE;
-if ($_SESSION['oConfig']->getAmbito() === Cargo::AMBITO_DL) {
-    $id_ambito_dl = TRUE;
-}
+ * Se entra como admin del servidor.
+ * No se puede distinguir si es el servidor de la dl o de los ctr.
+ * El servidor puede contener entidades que sean dl, y otras ctr
+ */
 
 $a_campos = [
     'lista_id_ctr' => $str_id_ctr,
-    'is_ambito_dl' => $id_ambito_dl,
 ];
 
 $oView = new ViewTwig('migration_ctr/controller');
