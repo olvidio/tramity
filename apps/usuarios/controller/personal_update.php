e11f8d8bcfa8423f3581b65222803cbe8d0652c3
dani <dani@dani-ThinkPad-T540p>
2023-11-08 20:56:25 +0100

    error al grabar preferencias

--- apps/usuarios/controller/personal_update.php
+++ apps/usuarios/controller/personal_update.php
@@ -1,5 +1,7 @@
-﻿<?php
-//namespace usuarios\controller;
+<?php
+
+use core\ConfigGlobal;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use usuarios\model\entity\GestorPreferencia;
 use usuarios\model\entity\Usuario;
 
@@ -11,58 +13,42 @@ require_once("apps/core/global_header.inc");
 require_once("apps/core/global_object.inc");
 // FIN de  Cabecera global de URL de controlador ********************************
 
-
-$Q_que = (string)filter_input(INPUT_POST, 'que');
-
+$error_txt = '';
 $gesPreferencias = new GestorPreferencia();
-switch ($Q_que) {
-    case "slickGrid":
-        $Q_tabla = (string)filter_input(INPUT_POST, 'tabla');
-        $Q_sPrefs = (string)filter_input(INPUT_POST, 'sPrefs');
-        $idioma = core\ConfigGlobal::mi_Idioma();
-        $tipo = 'slickGrid_' . $Q_tabla . '_' . $idioma;
-        $oPref = $gesPreferencias->getMiPreferencia($tipo);
-        // si no se han cambiado las columnas visibles, pongo las actuales (sino las borra).
-        $aPrefs = json_decode($Q_sPrefs, true);
-        if ($aPrefs['colVisible'] == 'noCambia') {
-            $sPrefs_old = $oPref->getMiPreferencia();
-            $aPrefs_old = json_decode($sPrefs_old, true);
-            $aPrefs['colVisible'] = empty($aPrefs_old['colVisible']) ? '' : $aPrefs_old['colVisible'];
-            $Q_sPrefs = json_encode($aPrefs, true);
-        }
-
-        $oPref->setPreferencia($Q_sPrefs);
-        if ($oPref->DBGuardar() === FALSE) {
-            echo _("hay un error, no se ha guardado");
-            echo "\n" . $oPref->getErrorTxt();
-        }
-        break;
-    default:
-        // Guardar idioma:
-        $Q_idioma_nou = (string)filter_input(INPUT_POST, 'idioma_nou');
-        $oPref = $gesPreferencias->getMiPreferencia('idioma');
-        $oPref->setPreferencia($Q_idioma_nou);
-        if ($oPref->DBGuardar() === FALSE) {
-            echo _("hay un error, no se ha guardado idioma");
-            echo "\n" . $oPref->getErrorTxt();
-        }
-
-        // Guardar Nombre a Mostrar, mail, cargo preferido
-        $id_usuario = core\ConfigGlobal::mi_id_usuario();
-        $Q_nom_usuario = (string)filter_input(INPUT_POST, 'nom_usuario');
-        $Q_email = (string)filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-        $Q_id_cargo_preferido = (integer)filter_input(INPUT_POST, 'id_cargo_preferido');
-
-        $oUsuario = new Usuario($id_usuario);
-        if ($oUsuario->DBCargar() === FALSE ){
-            $err_cargar = sprintf(_("OJO! no existe el usuario en %s, linea %s"), __FILE__, __LINE__);
-            exit ($err_cargar);
-        }
-        $oUsuario->setId_cargo_preferido($Q_id_cargo_preferido);
-        $oUsuario->setEmail($Q_email);
-        $oUsuario->setNom_usuario($Q_nom_usuario);
-        if ($oUsuario->DBGuardar() === FALSE) {
-            echo _("hay un error, no se ha guardado");
-            echo "\n" . $oUsuario->getErrorTxt();
-        }
+// Guardar idioma:
+$Q_idioma_nou = (string)filter_input(INPUT_POST, 'idioma_nou');
+$oPref = $gesPreferencias->getMiPreferencia('idioma');
+$oPref->setPreferencia($Q_idioma_nou);
+if ($oPref->DBGuardar() === FALSE) {
+    $error_txt .= _("hay un error, no se ha guardado idioma");
+    $error_txt .= "\n" . $oPref->getErrorTxt();
 }
+
+// Guardar Nombre a Mostrar, mail, cargo preferido
+$id_usuario = ConfigGlobal::mi_id_usuario();
+$Q_nom_usuario = (string)filter_input(INPUT_POST, 'nom_usuario');
+$Q_email = (string)filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+$Q_id_cargo_preferido = (integer)filter_input(INPUT_POST, 'id_cargo_preferido');
+
+$oUsuario = new Usuario($id_usuario);
+if ($oUsuario->DBCargar() === FALSE) {
+    $error_txt .= sprintf(_("OJO! no existe el usuario en %s, linea %s"), __FILE__, __LINE__);
+}
+
+$oUsuario->setId_cargo_preferido($Q_id_cargo_preferido);
+$oUsuario->setEmail($Q_email);
+$oUsuario->setNom_usuario($Q_nom_usuario);
+if ($oUsuario->DBGuardar() === FALSE) {
+    $error_txt .= _("hay un error, no se ha guardado");
+    $error_txt .= "\n" . $oUsuario->getErrorTxt();
+}
+
+if (empty($error_txt)) {
+    $jsondata['success'] = TRUE;
+    $jsondata['mensaje'] = 'ok';
+} else {
+    $jsondata['success'] = FALSE;
+    $jsondata['mensaje'] = $error_txt;
+}
+$response = new JsonResponse($jsondata);
+$response->send();
